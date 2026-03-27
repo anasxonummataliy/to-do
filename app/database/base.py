243cd52b2cbb2b0b8d3d2ad96117aa996e9b82ca
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-03-27 15:00:02 +0500

    fix

--- app/database/base.py
+++ app/database/base.py
@@ -1,15 +1,15 @@
 from app.database.session import async_engine
-from sqlalchemy import Column, DateTime, func
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
 
 class Base(DeclarativeBase):
     @declared_attr
     def create_at(cls):
-        return Column(DateTime, default=func.now())
+        return mapped_column(DateTime, default=func.now())
 
     @declared_attr
     def update_at(cls):
-        return Column(DateTime, default=func.now(), onupdate=func.now())
+        return mapped_column(DateTime, default=func.now(), onupdate=func.now())
 
 
 async def create_db_and_tables():
--- app/security/jwt.py
+++ app/security/jwt.py
@@ -1,14 +1,14 @@
 import jwt
 from datetime import  datetime, timedelta
 from fastapi import HTTPException
-from server.utils.config import settings
+from app.utils.config import settings
 
-SECRET_KEY = settings.jwt_secret  
+SECRET_KEY = settings.jwt_secret
 ALGORITHM = "HS256"
 
 
 def create_jwt_token(user_id: int) -> str:
-    expire = datetime.utcnow() + timedelta(days=7)  
+    expire = datetime.now() + timedelta(days=7)
     to_encode = {"id": user_id, "exp": expire}
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return token
