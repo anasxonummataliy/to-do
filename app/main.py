e1b526a19811cca1e0ddde7d6c39ec81b837e6a8
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-03-27 15:00:40 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi.security import HTTPBearer
-from server.database.base import create_db_and_tables
+from app.database.base import create_db_and_tables
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -10,17 +10,19 @@ from app.router.todo import router as todo_router
 
 auth_scheme = HTTPBearer(auto_error=False)
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await create_db_and_tables()
     yield
+
 
 app = FastAPI(
     title="Auth API",
     description="Auth API",
     version="1.0.0",
     lifespan=lifespan,
-    dependencies=[Depends(auth_scheme)]
+    dependencies=[Depends(auth_scheme)],
 )
 
 
@@ -31,9 +33,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/")
 async def start():
-    return {"message" : "Server is working!"}
+    return {"message": "Server is working!"}
+
 
 app.include_router(auth_router)
 app.include_router(todo_router)
